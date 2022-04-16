f29f56bc0651cc237ec3036c0fd079ff431455b7
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-04-16 19:12:10 +0530

    sec

---
+++ citycountry/src/components/AddCountry.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const AddCountry = () => {
+
+  return (
+    <h1>
+     
+     <form action="">
+            <label>Add Country</label>
+                 <br />
+            <input type="text" name="" id="" className='countryNameInp'/>
+                  <br />
+            <input type="submit" className='countryNameInp'/>
+        </form>
+
+    </h1>
+  
+  )
+}
+
+export default AddCountry
