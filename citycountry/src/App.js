74029d49f4346469016a52cd42346a65d3351ad2
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-04-17 08:55:12 +0530

    updated

--- citycountry/src/App.js
+++ citycountry/src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Router from './components/Router'
-
-
+import {Route, Routes} from 'react-router-dom'
+import {AddCountry} from "./components/AddCountry/addCountry"
+import {AddCity} from "./components/AddCity/addCity"
+import { Navbar } from "./components/Navbar/navbar";
+import {Home} from "./components/Hompage/Homepage"
+import {CityUpdate} from  "./components/update/cityupdate"
 function App() {
   return (
     <div className="App">
-            <Router/>
+      <Navbar/>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route exact path="/add-country" element={<AddCountry/>}/>
+          <Route exact path="/add-city" element={<AddCity/>}/>
+          <Route path="/update/:id" element={<CityUpdate />} />
+      </Routes>
     </div>
   );
 }
